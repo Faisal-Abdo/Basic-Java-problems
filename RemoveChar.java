dd6246cfb7083294793db1680a1e586af5b62a19
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-09-24 13:30:51 +0400

    Added Remove Character question

---
+++ RemoveChar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RemoveChar {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("enter a string: ");
+        String input1 = obj.nextLine();
+        System.out.print("enter a character you want to remove: ");
+        char inputChar = obj.next().charAt(0);
+        RemoveCharacter(input1,inputChar);
+    }
+    public static void RemoveCharacter(String str, char ch){
+        char[] stringtoArray = str.toCharArray();
+        StringBuilder newString = new StringBuilder(); //Class used for creating and manipulating mutable strings
+        for(char x: stringtoArray){
+            if(x!=ch){
+                newString.append(x);
+            }}
+        System.out.println(newString);
+    }}
