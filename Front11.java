f8accb3d4e0b762fd885e2bda59ee77b03db882a
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-09-29 11:27:00 +0400

    Added Front 11 question

---
+++ Front11.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Front11 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter your preferred size of 1st array: ");
+        int size1 = obj.nextInt();
+        System.out.println("Enter " + size1 + " integers: ");
+        int[] Array1 = new int[size1];
+        for (int i = 0; i < size1; i++) {
+            if (obj.hasNextInt()) {
+                Array1[i] = obj.nextInt();
+            }
+        }
+        System.out.println("Enter your preferred size of 2nd array: ");
+        int size2 = obj.nextInt();
+        System.out.println("Enter " + size2 + " integers: ");
+        int[] Array2 = new int[size2];
+        for (int i = 0; i < size2; i++) {
+            if (obj.hasNextInt()) {
+                Array2[i] = obj.nextInt();
+            }
+        }
+        front11(Array1,Array2);
+    }
+        public static int[] front11(int[] array, int[] array1){
+            if(array.length>0 && array1.length>0){
+                System.out.println("["+array[0]+","+array1[0]+"]");
+            } else if (array.length>0 && array1.length<1) {
+                System.out.println("["+array[0]+"]");
+            } else if (array.length<1 && array1.length>0) {
+                System.out.println("["+array1[0]+"]");
+            }
+            return null;
+    }
+}
