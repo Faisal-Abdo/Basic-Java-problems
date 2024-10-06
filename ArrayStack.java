72d770777df207359ef2c55df00c66d3ccd036ab
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-10-06 19:17:07 +0400

    Added Implement a Stack Using an Array question

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,20 +1,34 @@
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class ArrayStack {
     public static void main(String[] args) {
-        Stack<Integer> random = new Stack<>();
-        random.push(5);
-        random.push(10);
-        random.push(3);
 
-        Integer pop = random.pop();
-        System.out.println(pop);
+        ArrayList<Integer> random = new ArrayList<>();
+        push(random,5);
+        push(random,10);
+        push(random,9);
 
-        Integer peeking = random.peek();
-        System.out.println(peeking);
-
-        boolean empty= random.isEmpty();
-        System.out.println(empty);
+        pop(random);
+        peek(random);
+        checkingEmpty(random);
 
     }
+
+    public static void push(ArrayList<Integer> array, int n){
+        array.add(n);
+        System.out.println(array);
+    }
+    public static void pop(ArrayList<Integer> array){
+        int removal = array.get(0);
+        array.removeFirst();
+        System.out.println(array);
+    }
+    public static void peek(ArrayList<Integer> array){
+        System.out.println(array.get(0));
+    }
+    public static void checkingEmpty(ArrayList<Integer> array){
+        System.out.println(array.isEmpty());
+    }
+
 }
