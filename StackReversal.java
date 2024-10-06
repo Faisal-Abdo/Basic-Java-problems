ec223e7dd21793e888249450ae57680edcd3c038
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-10-06 22:49:51 +0400

    Added Reverse a String Using a Stack question

---
+++ StackReversal.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class StackReversal {
+    public static void main(String[] args) {
+        Stack<String> books = new Stack<>();
+        reverseString("Hello");
+    }
+    public static void reverseString(String word){
+
+        Stack<String> reverse = new Stack<>();
+        char[] stringTOChar = word.toCharArray();
+        /*String str = new String(stringTOChar);
+        String con  = Arrays.toString(str); */
+        String empty = "";
+        for(char a: stringTOChar){
+            reverse.push(Character.toString(a));
+        }
+        for (char b: stringTOChar){
+            empty+=reverse.pop();
+        }
+
+        System.out.println(empty);
+
+    }
+}
