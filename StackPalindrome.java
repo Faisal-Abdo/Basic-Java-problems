36df2ec8d895d5544cc324cc3ee7820d001003ff
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-10-08 10:20:09 +0400

    updated Check for Palindrome Using Stack question

--- StackPalindrome.java
+++ StackPalindrome.java
@@ -2,21 +2,21 @@ import java.util.Stack;
 
 public class StackPalindrome {
     public static void main(String[] args) {
-        palindromeChecker("ababa");
+        palindromeChecker("racecar");
     }
     public static boolean palindromeChecker(String word){
         boolean flag = false;
         Stack<String> reverse = new Stack<>();
         char[] stringTOChar = word.toCharArray();
-        String empty = "";
-        String empty1 = "";
+        StringBuilder originWord = new StringBuilder();
+        StringBuilder reverseWord = new StringBuilder();
         for(char a: stringTOChar){
-            empty+=reverse.push(Character.toString(a));
+            originWord.append(reverse.push(Character.toString(a)));
         }
         for (char b: stringTOChar){
-            empty1+=reverse.pop();
+            reverseWord.append(reverse.pop());
         }
-        if(empty1.equals(empty)){
+        if(reverseWord.toString().equals(originWord.toString())){
             flag = true;
         }
         System.out.println(flag);
