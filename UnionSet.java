e61a4e68851b398fbd91ee3970b2f3cdfb66f6e8
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-10-09 12:13:17 +0400

    updated Union of Two Arrays Question

--- UnionSet.java
+++ UnionSet.java
@@ -4,13 +4,22 @@ import java.util.Set;
 
 public class UnionSet {
     public static void main(String[] args) {
-        Set<Integer> setA = new HashSet<>(Set.of(2,4,3));
-        Set<Integer> setB = new HashSet<>(Set.of(5,7,3,1));
-        findUnion(setA,setB);
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(2);
+        arr.add(4);
+        arr.add(3);
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        arr1.add(5);
+        arr1.add(7);
+        arr1.add(3);
+        arr1.add(1);
+        findUnion(arr,arr1);
     }
-    public static Set<Integer> findUnion(Set<Integer> set, Set<Integer> set1){
-        set.addAll(set1);
-        System.out.println(set);
-        return set;
+    public static Set<Integer> findUnion(ArrayList<Integer> array, ArrayList<Integer> array1){
+        Set<Integer> setA = new HashSet<>(array);
+        Set<Integer> setB = new HashSet<>(array1);
+        setA.addAll(setB);
+        System.out.println(setA);
+        return setA;
     }
 }
