e5cabb1887484f5f2a92c564fde1588ec6768a5a
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-10-10 11:38:13 +0400

    Updated Common Characters in Strings Question

--- CommonCharacters.java
+++ CommonCharacters.java
@@ -9,16 +9,12 @@ public class CommonCharacters {
     public static void findCommon(String word, String word1){
         char[] StringToChar = word.toCharArray();
         char[] StringToChar1 = word1.toCharArray();
-        ArrayList<Character> stringArray = new ArrayList<>();
-        ArrayList<Character> stringArray1 = new ArrayList<>();
+        Set<Character> set = new HashSet<>();
+        Set<Character> set1 = new HashSet<>();
         for(int i=0;i<StringToChar.length;i++){
-            stringArray.add(StringToChar[i]);
-            stringArray1.add(StringToChar1[i]);
+            set.add(StringToChar[i]);
+            set1.add(StringToChar1[i]);
         }
-
-
-        Set<Character> set = new HashSet<>(stringArray);
-        Set<Character> set1 = new HashSet<>(stringArray1);
         set.retainAll(set1);
         System.out.println(set);
     }
