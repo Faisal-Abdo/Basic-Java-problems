d752e6ea6b1b1ff850841cda95e0e6a5956beba4
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-10-13 12:17:29 +0400

    Added Find Missing Number Question

---
+++ MissingNumber.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(3,0,1)); //in the case that there is one missing number
+        findMissingNum(array);
+    }
+    public static Integer findMissingNum(ArrayList<Integer> arr){
+        TreeSet<Integer> treeSet = new TreeSet<>(arr);
+        Integer missingNum = 0;
+        for(Integer a: treeSet){
+            missingNum = a+1;  //the ultimate logic
+            if(!treeSet.contains(missingNum)){
+                break;
+            }
+        }
+        System.out.println(missingNum);
+        return missingNum;
+    }
+}
