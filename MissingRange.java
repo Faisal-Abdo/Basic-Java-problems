d6cafbc442daffbf89793e0c55ee1a649b191440
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-10-14 11:21:15 +0400

    Updated Find Missing Elements of a Range Question

--- MissingRange.java
+++ MissingRange.java
@@ -4,19 +4,26 @@ import java.util.TreeSet;
 
 public class MissingRange {
     public static void main(String[] args) {
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(3,0,1)); //in the case that there is one missing number
-        findMissingNum(array);
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1,3,5,7)); //in the case that there is one missing number
+        Integer low = 1;
+        Integer high = 10;
+        findMissingNum(array, low, high);
     }
-    public static Integer findMissingNum(ArrayList<Integer> arr){
-        TreeSet<Integer> treeSet = new TreeSet<>(arr);
-        Integer missingNum = 0;
-        for(Integer a: treeSet){
-            missingNum = a+1;  //the ultimate logic
-            if(!treeSet.contains(missingNum)){
-                break;
-            }
+    public static TreeSet<Integer> findMissingNum(ArrayList<Integer> arr, Integer min, Integer max){
+        ArrayList<Integer> newArray = new ArrayList<>();
+        for(int m=0; m<=max-1;m++){
+            newArray.add(null); //we set initial size to max(to make the list dynamic)
         }
-        System.out.println(missingNum);
-        return missingNum;
-    }
-}
+        for(int k=0; k< arr.size();k++){
+            newArray.set(k,arr.get(k));  //we add the given/existing elements
+        }
+        TreeSet<Integer> missingNumSet = new TreeSet<>();
+        Integer missingNum = 0;
+        for(int i=min; i>=min && i<=max;i++){
+            missingNum = i;  //the ultimate logic
+            if(!newArray.contains(missingNum)){  //this way we could go through all the indices in the range
+                missingNumSet.add(missingNum);
+            }}
+        System.out.println(missingNumSet);
+        return missingNumSet;
+    }}
